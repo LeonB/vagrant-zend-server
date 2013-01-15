782b11592bfc5c61a3fd4c0254181873c802a443
Leon Bogaert <leon@tim-online.nl>
2013-01-15 16:25:05 +0100

    Correct directory for `php_custom.ini`

--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -1,6 +1,6 @@
 class php {
 
-	file { '/etc/php5/fpm/conf.d/php_custom.ini':
+	file { '/usr/local/zend/etc/conf.d/php_custom.ini':
 		content => template("php/php_custom.ini.erb"),
 		ensure  => present,
 		owner   => 'root',
